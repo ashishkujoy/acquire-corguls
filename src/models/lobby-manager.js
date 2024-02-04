017a5c8be5e6c7e246aa9fc5086317ccc05d01df
BiswajitSen <sen229126@gmail.com>
2024-02-04 22:44:51 +0530

    [000] Biswajit | Fix corporation size incement, on Merger

---
+++ src/models/lobby-manager.js
@@ -0,0 +1,24 @@
+const Lobby = require("./lobby");
+
+class LobbyManager {
+  #lobbies;
+  #idGenerator;
+  constructor(lobbies, idGenerator) {
+    this.#lobbies = lobbies;
+    this.#idGenerator = idGenerator;
+  }
+
+  createLobby(size) {
+    const lobby = new Lobby(size);
+    const id = this.#idGenerator.generate();
+    this.#lobbies[id] = lobby;
+
+    return id;
+  }
+
+  findById(id) {
+    return this.#lobbies[id];
+  }
+}
+
+module.exports = LobbyManager;
---
+++ test/models/lobby-generator-test.js
@@ -0,0 +1,34 @@
+const { describe, it } = require("node:test");
+const LobbyManager = require("../../src/models/lobby-manager");
+const sinon = require("sinon");
+const assert = require("node:assert");
+const Lobby = require("../../src/models/lobby");
+
+describe("Lobby Manager", () => {
+  it("create a new lobby", () => {
+    const idGenerator = { generate: sinon.stub().returns("abcd") };
+    const lobbyManager = new LobbyManager({}, idGenerator);
+
+    const lobbyId = lobbyManager.createLobby({ lowerLimit: 2, upperLimit: 6 });
+    assert.equal(lobbyId, "abcd");
+    assert(idGenerator.generate.calledOnce);
+  });
+
+  it("find existing lobby by id", () => {
+    const lobby = new Lobby({ lowerLimit: 2, upperLimit: 6 });
+    const lobbyManager = new LobbyManager({ "abcd": lobby }, {});
+
+    const actualLobby = lobbyManager.findById("abcd");
+
+    assert.deepEqual(actualLobby, lobby);
+  });
+
+  it("find non existing lobby by id", () => {
+    const lobby = new Lobby({ lowerLimit: 2, upperLimit: 6 });
+    const lobbyManager = new LobbyManager({ "abcd": lobby }, {});
+
+    const actualLobby = lobbyManager.findById("abce");
+
+    assert.equal(actualLobby, undefined);
+  });
+});
